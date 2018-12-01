9d6985a45523697565d737c9d9b1ee6a8b2c7860
Laura Morales <eneyra29@gmail.com>
2018-11-30 21:53:11 -0600

    primera práctica de Java Script

---
+++ HTML/clase-js.js
@@ -0,0 +1,54 @@
+/*console.log('Ya cargó el JS');
+console.error('TODO MAAAAAAL');*/
+//esto es un comentario de una línea
+
+var edad=5;
+ 
+if(edad>=18){
+    console.log('Mayor de edad');
+
+    if(edad>=18 & edad<=24){
+        console.log('Universidad');
+    }else{
+        if(edad>=25 & edad<=28){
+            console.log('Posgrado');
+        }else{
+            if(edad>=29 & edad<=33){
+                console.log('Adulto joven');
+            }else{
+                if(edad>=34 & edad<=40){
+                    console.log('Chavorruco');
+                }else{
+                    if(edad>=41 & edad<=47){
+                        console.log('Señor crisis');
+                    }else{
+                        if(edad>=48 & edad<=60){
+                            console.log('Viejo lesbiano');
+                        }else{
+                            if(edad>61){
+                                console.log('Sugar Daddy');
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}else{
+    console.log('Menor de edad');
+
+    if(edad>=6 & edad<=12){
+        console.log('Primaria');
+    }else{
+        if(edad>=13 & edad<=15){
+            console.log('Secundaria');
+        }else{
+            if(edad>=16 & edad<=17){
+                console.log('Prepa');
+            }
+        }
+    }
+}
+
+
