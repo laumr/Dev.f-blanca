4497eb1a0f5a7f2dd88abb410b30994cfd7e4371
Laura Morales <eneyra29@gmail.com>
2018-12-07 15:27:53 -0600

    Estilos de mo hermosa calculadora

---
+++ HTML/calculadora/calc.js
@@ -0,0 +1,11 @@
+/**
+ * Esta práctica genera una calculadora simple
+ * @author Laumr2991
+*/
+
+
+function agregarApantalla(valor){
+    var impresion=document.getElementById('laPantalla');
+    impresion.innerText=valor;
+}
+
