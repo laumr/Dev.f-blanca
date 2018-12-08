4ec08054beca70a8763009485609ab3252d63d1c
Laura Morales <eneyra29@gmail.com>
2018-12-07 18:02:54 -0600

    comienzo de javascript

--- HTML/calculadora/calc.js
+++ HTML/calculadora/calc.js
@@ -4,8 +4,39 @@
 */
 
 
-function agregarApantalla(valor){
-    var impresion=document.getElementById('laPantalla');
-    impresion.innerText=valor;
+function agregarApantalla(x){
+    var valoractual=document.getElementById('laPantalla').innerText; 
+    if(valoractual.length<10){
+    var impresion=document.getElementById('laPantalla');  
+    impresion.innerText=`${valoractual}${x}`;
+    }
 }
 
+function guardar(content){
+    var primernumero=getElementById('laPantalla').value;
+    console.log(primernumero);
+//aquí poner ifs depende de la operación, manda a la siguiente función y en esa función agarra este y el valor de la patalla.s
+}
+
+function borrar(){
+    var valoractual=document.getElementById('laPantalla');
+    valoractual.innerText='';
+}
+
+function suma(x,y){
+    return x+y;
+}
+
+function resta(x,y){
+    return x-y;
+}
+
+function multiplicacion(x,y){
+    return x*y;
+}
+
+function division(x,y){
+    return x/y;
+}
+
+//Math.sqrt()
