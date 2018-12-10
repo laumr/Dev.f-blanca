b0eb87c707620c1b070fd4298a04f41ae5559737
Laura Morales <eneyra29@gmail.com>
2018-12-09 21:27:07 -0600

    caluladora al 99.99999999

--- HTML/calculadora/calc.js
+++ HTML/calculadora/calc.js
@@ -2,19 +2,24 @@
  * Esta práctica genera una calculadora simple
  * @author Laumr2991
 */
-//Falta -  que el punto no me lo cuente, que no borre cuando pico operación.
+/** Falta -  que el punto no me lo cuente, que acorte resultados a 12 en suma y multi dando el resultado correcto
+ * 
+ * */
 
 var primerNumero;
 var segundoNumero;
 var operacion;
 var valorActual;
 var resultado;
+var cuenta=0;
+
 
 
 function agregarApantalla(x){ 
     var valorPantalla=document.getElementById('laPantalla').innerText;
-    if(valorPantalla=='0'||operacion.length>0){
+    if(valorPantalla=='0'|| cuenta==1){
         borrar();
+        cuenta=0;
     }
     valorActual=document.getElementById('laPantalla').innerText; 
     if(valorActual.length<10){
@@ -35,7 +40,7 @@ function imprimirPunto(){
 function guardar(signo){
     primerNumero=document.getElementById('laPantalla').innerText;
     operacion=signo
-    
+    cuenta=1;
 }
 
 function resultadoCorto(){
@@ -43,8 +48,10 @@ function resultadoCorto(){
     if(resultado.length>12){
         resultado=resultado.slice(0,11);
         agregarApantalla(resultado);
+        cuenta=1;
     }else{
         agregarApantalla(resultado);
+        cuenta=1;
     }
 }
 
@@ -61,21 +68,25 @@ function igualA(){
         resultado=suma(Number(primerNumero),Number(segundoNumero));
         borrar();
         agregarApantalla(resultado);
+        cuenta=1;
     }else{
         if(operacion=='-'){
             resultado=resta(Number(primerNumero),Number(segundoNumero));
             borrar();
             agregarApantalla(resultado);
+            cuenta=1;
         }else{
             if(operacion=='x'){
                 resultado=multiplicacion(Number(primerNumero),Number(segundoNumero));
                 borrar();
                 agregarApantalla(resultado);
+                cuenta=1;
             }else{
                 if(operacion=='/'){
                     resultado=division(Number(primerNumero),Number(segundoNumero));
                     borrar();
                     resultadoCorto();
+                    cuenta=1;
                 }    
                 
             }
@@ -88,11 +99,10 @@ function borrar(){
     valorPantalla.innerText='';
 }
 
-function sustituir(){
-    var valorPantalla=document.getElementById('laPantalla').innerText;
-    if(valorPantalla=='0'){
-        borrar();
-    }
+function borrarC(){
+    var valorPantalla=document.getElementById('laPantalla');
+    valorPantalla.innerText='0';
+    cuenta=0;
 }
 
 function suma(x,y){
@@ -118,3 +128,5 @@ function raiz(x){
 function redondear(x){
     return Math.round(x);
 }
+
+
